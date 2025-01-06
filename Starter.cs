1208f92d78055c1bc75cf8d02471f26a58de96f7
Dr26zlo <artabr.301@gmail.com>
2025-01-06 18:30:59 +0500

    Starter.cs

---
+++ Starter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+internal class Starter
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new MainForm());
+    }
+}
